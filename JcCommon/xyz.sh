859af3ce3672ef7e04109d5d002a3428ca15c41c
JavaCommons Technologies <javacommons@gmail.com>
2025-04-09 22:28:08 +0900

    .

--- JcCommon/xyz.sh
+++ JcCommon/xyz.sh
@@ -1,9 +1,11 @@
 #! /usr/bin/env bash
 set -uvx
 set -e
-qmake6 -project -norecursive -t lib -o xyz.pro
-echo CONFIG += dll >> xyz.pro
-echo QMAKE_LIBS += -lgraphite2 -lbz2  -lrpcrt4 -lusp10 >> xyz.pro
-echo DESTDIR = \$\$PWD >> xyz.pro
+ts=`date "+_%Y_%m%d_%H%M_%S_"`
+qmake6 -project -norecursive -t lib -o xyz$ts.pro
+echo CONFIG += dll >> xyz$ts.pro
+echo QMAKE_LIBS += -lgraphite2 -lbz2  -lrpcrt4 -lusp10 >> xyz$ts.pro
+echo DESTDIR = \$\$PWD >> xyz$ts.pro
 rm -rf build
-pro64 -s xyz.pro
+pro64 -s xyz$ts.pro
+rm -f *.pro
