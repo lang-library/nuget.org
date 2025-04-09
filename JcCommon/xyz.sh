f4cc2b57812f939af02e6afe60c0e8a93e762553
JavaCommons Technologies <javacommons@gmail.com>
2025-04-09 23:19:40 +0900

    .

--- JcCommon/xyz.sh
+++ JcCommon/xyz.sh
@@ -4,6 +4,7 @@ set -e
 ts=`date "+_%Y_%m%d_%H%M_%S_"`
 qmake6 -project -norecursive -t lib -o xyz$ts.pro
 echo CONFIG += dll >> xyz$ts.pro
+echo INCLUDEPATH += \$\(HOME\)/common/include >> xyz$ts.pro
 echo QMAKE_LIBS += -lgraphite2 -lbz2  -lrpcrt4 -lusp10 >> xyz$ts.pro
 echo DESTDIR = \$\$PWD >> xyz$ts.pro
 rm -rf build
