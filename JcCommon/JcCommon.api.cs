eaff72578574fb71d2d208f8f9c8e77b789e3579
JavaCommons Technologies <javacommons@gmail.com>
2025-04-09 11:30:08 +0900

    .

---
+++ JcCommon/JcCommon.api.cs
@@ -0,0 +1,14 @@
+//css_nuget EasyObject
+using static Global.EasyObject;
+
+namespace JcCommon;
+
+public static class Api
+{
+    public static int Add2(int a, int b)
+    {
+        Echo(a, "a");
+        Echo(b, "b");
+        return a + b;
+    }
+}
