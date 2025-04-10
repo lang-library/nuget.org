1867120c8d86ddcc5fa2dc48c3c9a9be78cd731c
JavaCommons Technologies <javacommons@gmail.com>
2025-04-10 18:20:10 +0900

    .

--- QProcess.Win64/gen.sh
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env bash
-set -uvx
-set -e
-rm -rf *.dll
-swig -csharp -c++ -cppext cpp -outdir . QProcess_Win64.i
-dll-gen QProcess_Win64.cpp
-pro64 -s QProcess_Win64.pro
-cp -p QProcess_Win64.x86_64-static.dll QProcess_Win64.dll
-cp -p QProcess_Win64.dll ~/cmd/
--- QProcess.Win64/mk-dll.sh
+++ QProcess.Win64/mk-dll.sh
@@ -1,12 +1,9 @@
 #! /usr/bin/env bash
 set -uvx
 set -e
+rm -rf *.dll
 swig -csharp -c++ -cppext cpp -outdir . QProcess_Win64.i
-qmake6 -project -norecursive -t lib -o QProcess_Win64.pro
-echo CONFIG += dll >> QProcess_Win64.pro
-echo INCLUDEPATH += \$\(HOME\)/common/include >> QProcess_Win64.pro
-echo QMAKE_LIBS += -lgraphite2 -lbz2  -lrpcrt4 -lusp10 >> QProcess_Win64.pro
-echo DESTDIR = \$\$PWD >> QProcess_Win64.pro
-rm -rf build
+dll-gen QProcess_Win64.cpp
 pro64 -s QProcess_Win64.pro
-cp QProcess_Win64.dll ~/cmd/
+cp -p QProcess_Win64.x86_64-static.dll QProcess_Win64.dll
+cp -p QProcess_Win64.dll ~/cmd/
