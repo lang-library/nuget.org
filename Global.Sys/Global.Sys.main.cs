00ee0ef2ff20521661c5f1b788709df51d9d850f
JavaCommons Technologies <javacommons@gmail.com>
2025-04-14 00:07:06 +0900

    .

--- Global.Sys/Global.Sys.main.cs
+++ Global.Sys/Global.Sys.main.cs
@@ -1,15 +1,59 @@
 //css_inc Global.Sys.cs
 //css_nuget EasyObject
-namespace Global;
-
+//css_embed add2.dll
+using System;
+using System.Runtime.InteropServices;
 using static Global.EasyObject;
 
-public class Program
+namespace Global
 {
-    public static void Main(string[] args)
+    public class Program
     {
-        Log(args, "args");
-        Echo("helloハロー©");
-        Echo(Sys.Add2(11, 22));
+        delegate int proto_add2(int a, int b);
+        delegate IntPtr proto_greeting(IntPtr name);
+        public static void Main(string[] args)
+        {
+            Log(args, "args");
+            Log(Sys.CheckFixedArguments("dummy", 1, args));
+            Echo("helloハロー©");
+            string projFileName = @"D:\home09\api\nlohmann\main.cpp";
+            string dllPath = Installer.InstallResourceDll(
+                typeof(Program).Assembly,
+                "C:\\dll-dir",
+                "Global.Sys:add2.dll");
+            Echo(dllPath, "dllPath");
+            IntPtr Handle = IntPtr.Zero;
+            Handle = Sys.LoadLibraryExW(
+                dllPath,
+                IntPtr.Zero,
+                Sys.LoadLibraryFlags.LOAD_WITH_ALTERED_SEARCH_PATH
+                );
+            if (Handle == IntPtr.Zero)
+            {
+                EasyObject.Log($"DLL not loaded: {dllPath}");
+                Environment.Exit(1);
+            }
+            CallAdd2(Handle);
+            CallGreeting(Handle);
+            Echo(Sys.RunCommand("ping", "-n", "2", "www.youtube.com"));
+            JsonApiClient jc = new JsonApiClient("cs-api.dll");
+            Echo(jc.Call("add2", EasyObject.FromObject(new object[] { 1111, 2222 })));
+            //Echo(jc.Call("add2", EasyObject.FromObject(new object[] { 1111, 2222, 3333 })));
+        }
+        private static void CallAdd2(IntPtr Handle)
+        {
+            IntPtr Add2Ptr = Sys.GetProcAddress(Handle, "add2");
+            proto_add2 add2 = (proto_add2)Marshal.GetDelegateForFunctionPointer(Add2Ptr, typeof(proto_add2));
+            Echo(add2(11, 22));
+        }
+        private static void CallGreeting(IntPtr Handle)
+        {
+            IntPtr GreetingPtr = Sys.GetProcAddress(Handle, "greeting");
+            proto_greeting greeting = (proto_greeting)Marshal.GetDelegateForFunctionPointer(GreetingPtr, typeof(proto_greeting));
+            IntPtr namePtr = Sys.StringToUTF8Addr("トム©");
+            IntPtr result = greeting(namePtr);
+            Sys.FreeHGlobal(namePtr);
+            Echo(Sys.UTF8AddrToString(result));
+        }
     }
 }
