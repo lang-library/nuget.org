a120814970098a2ae8621eaf4330f8389f3ae801
JavaCommons Technologies <javacommons@gmail.com>
2025-04-15 11:20:11 +0900

    .

--- Global.Sys/Global.Sys.cs
+++ Global.Sys/Global.Sys.cs
@@ -110,8 +110,10 @@ namespace Global
             foreach (string test in PATH.Split(';'))
             {
                 string path = test.Trim();
+#if false
                 if (!String.IsNullOrEmpty(path) && File.Exists(Path.Combine(path, "dotnet4", exe)))
                     return Path.GetFullPath(Path.Combine(path, "dotnet4", exe));
+#endif
                 if (!String.IsNullOrEmpty(path) && File.Exists(Path.Combine(path, exe)))
                     return Path.GetFullPath(Path.Combine(path, exe));
                 string baseName = Path.GetFileNameWithoutExtension(exe);
